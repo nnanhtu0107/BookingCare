fccda414c85db90c5465a17d34e925df490a3897
nnanhtu0107 <nnanhtu017@gmail.com>
2021-11-07 18:06:31 +0700

    URL_REACT

--- src/server.js
+++ src/server.js
@@ -1,10 +1,36 @@
 import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
-import initWebRouter from './route/web';
+import initWebRouter from "./route/web";
 import connetDB from "./config/connectDB";
-require('dotenv').config();
+// import cors from "cors";
+require("dotenv").config();
 let app = express();
+// app.use(cors({ origin: true }));
+// app.use(cors());
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", process.env.URL_REACT);
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 //config app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,9 +39,8 @@ viewEngine(app);
 initWebRouter(app);
 connetDB();
 
-
 let port = process.env.PORT || 1234;
 //Port == undefined => port = 1234
 app.listen(port, () => {
-    console.log("Backend nodejs is running on the port:" + port)
-})
+  console.log("Backend nodejs is running on the port:" + port);
+});
