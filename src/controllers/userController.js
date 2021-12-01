dd81b28cfc3751388f5eec8534708af36d9be826
nnanhtu0107 <nnanhtu017@gmail.com>
2021-12-01 19:53:58 +0700

    get allcode api with node js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -35,23 +35,38 @@ let handleCreateNewUser = async (req, res) => {
   let message = await userService.createNewUser(req.body);
   return res.status(200).json(message);
 };
-let handleEditUser = async (req, res) =>{
-    let data = req.body;
-    let message = await userService.updateUserData(data);
-    return res.status(200).json(message)
-}
-let handleDeleteUser = async(req, res) =>{
-  if(!req.body.id){
+let handleEditUser = async (req, res) => {
+  let data = req.body;
+  let message = await userService.updateUserData(data);
+  return res.status(200).json(message);
+};
+let handleDeleteUser = async (req, res) => {
+  if (!req.body.id) {
     return res.status(200).json({
       errCode: 1,
-      errMessage: "Missing required parameters!"
-    })
+      errMessage: "Missing required parameters!",
+    });
   }
   let message = await userService.deleteUser(req.body.id);
   return res.status(200).json(message);
-}
+};
+let getAllCode = async (req, res) => {
+  try {
+    let data = await userService.getAllCodeService();
+    return res.status(200).json(data);
+  } catch (e) {
+    console.log("get all code err", e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from server",
+    });
+  }
+};
 module.exports = {
   handleLogin,
   handleGetAllUser,
-  handleCreateNewUser,handleEditUser,handleDeleteUser
+  handleCreateNewUser,
+  handleEditUser,
+  handleDeleteUser,
+  getAllCode,
 };
